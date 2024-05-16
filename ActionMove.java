1be1a8a9540a78433c3c520a7aa89a444e27d872
J20262030 <156226868+J20262030@users.noreply.github.com>
2024-05-16 08:40:37 -0700

    Create ActionMove.java

---
+++ ActionMove.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+public class ActionMove extends ActionCard{
+    //card that is randomly drawn, interspaced with the trivia questions
+    public ActionMove(String temp){
+        super(temp);
+    }
+    public void card(Graphics g){
+
+    }
+        //draws card, with text
+}
