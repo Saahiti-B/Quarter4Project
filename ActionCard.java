1f803cc6547822ed066f47592c620f4d953ae2bc
J20262030 <156226868+J20262030@users.noreply.github.com>
2024-05-16 08:40:05 -0700

    Create ActionCard.java

---
+++ ActionCard.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics;
+public abstract class ActionCard{
+    //trivia card or move back/forward
+    public ActionCard(String type){
+
+    }
+
+    public abstract void card(Graphics g);
+}
