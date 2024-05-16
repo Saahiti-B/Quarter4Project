daa08c3d7836ff79db02328b313012105b80b52e
J20262030 <156226868+J20262030@users.noreply.github.com>
2024-05-16 08:43:47 -0700

    Create TriviaQuestions.java

---
+++ TriviaQuestions.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+public class TriviaQuestions extends ActionCard{
+//trivia questions are multiple choice
+
+    public TriviaQuestions(String temp){
+        super(temp);
+    }
+    public void card(Graphics g){
+
+    }
+}
