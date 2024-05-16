1945c378d93fa63cf26bc4630848b0b76c111612
J20262030 <156226868+J20262030@users.noreply.github.com>
2024-05-16 08:38:47 -0700

    Update JavaLand.java

--- JavaLand.java
+++ JavaLand.java
@@ -1,1 +1,19 @@
+//backend
+import java.lang.Math;
+public class JavaLand{
+    //write methods to keep track of game (players, points, positions, etc.)
+    //write dice method (rolling, not drawing)
+    public JavaLand(){
+        
+    }
 
+    public void startScreen(){
+        
+    }
+
+    public int diceRolled(){
+        //use 1 die
+        return (int) (Math.random() * 6 + 1);
+    }
+    
+}
