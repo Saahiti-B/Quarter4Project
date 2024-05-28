ac254b2e3064117f0db10133ba10b9a4c8eac04f
J20262030 <156226868+J20262030@users.noreply.github.com>
2024-05-28 09:37:24 -0700

    Update TriviaQuestions.java

--- TriviaQuestions.java
+++ TriviaQuestions.java
@@ -1,19 +1,33 @@
 import java.awt.Graphics;
+import java.awt.Color;
+
 public class TriviaQuestions extends ActionCard{
 //trivia questions are multiple choice
-    String questions[][];
+    //ArrayList<String> questions;
+    String[] questions;
+    private Color temp;
+    private int x;
+    private int y;
 
-    public TriviaQuestions(String temp){
-        super(temp);
-         questions = {
-                "What is a word used to describe cold coffee? a)hot b)iced c) frothy", //iced
-                "What is a word for a machine used to make foamy milk? a)roaster b)espresso c)frother", //frother
-                "What is a word used to describe a ceramic cup often used for coffee? a)tumbler b)mug c)bowl", //mug
-                "What is a word to describe a coffee seed? a)bag b)box c)bean" //bean
-                "What is the main ingredient that is used for alertness in coffee? a)chocolate b)milk c)caffeine" //caffeine
-        };
+    public TriviaQuestions(Color temp, int x, int y){
+        super(temp, x, y);
+        //questions = new ArrayList<String>();
+        //questions.add("What is a word used to describe cold coffee? a)hot b)iced c) frothy");
+        //questions.add("What is a word for a machine used to make foamy milk? a)roaster b)espresso c)frother");
+        //questions.add()
+
+        questions = new String[5];
+        questions[0] = "What is a word used to describe cold coffee? a)hot b)iced c) frothy"; //iced
+        questions[1] = "What is a word for a machine used to make foamy milk? a)roaster b)espresso c)frother"; //frother
+        questions[2] = "What is a word used to describe a ceramic cup often used for coffee? a)tumbler b)mug c)bowl"; //mug
+        questions[3] =  "What is a word to describe a coffee seed? a)bag b)box c)bean"; //bean
+        questions[4] = "What is the main ingredient that is used for alertness in coffee? a)chocolate b)milk c)caffeine"; //caffeine
     }
-    public void card(Graphics g){
+    public void drawCard(Graphics g){
+
+    }
+
+    public void drawType(Graphics g){
 
     }
 
