8deb8455eff972b9441573b4e00238c475991646
Saahiti-B <165538451+Saahiti-B@users.noreply.github.com>
2024-05-30 09:51:22 -0700

    Update ActionCard.java

--- ActionCard.java
+++ ActionCard.java
@@ -1,9 +1,48 @@
 import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.*;
+
 public abstract class ActionCard{
+    private Color cardColor;
+	private int x;
+	private int y;
+
     //trivia card or move back/forward
-    public ActionCard(String type){
+    public ActionCard(Color cardColor, int x, int y){
+		this.cardColor = cardColor;
+		this.x = x;
+		this.y = y;
 
-    }
+	}
 
-    public abstract void card(Graphics g);
+    public void drawMe(Graphics g){
+		drawCard(g);
+		drawType(g);
+	}
+
+
+	public int getX(){
+		return x;
+	}
+
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public Color getCardColor(){
+		return cardColor;
+	}
+
+	public void setColor(Color color){
+		cardColor = color;
+	}
+
+	public abstract void drawCard(Graphics g);
+	
+	public abstract void drawType(Graphics g);
 }
