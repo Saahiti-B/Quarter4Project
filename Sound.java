0df89994df11afa2e65ee59dbfef91817c53a549
J20262030 <156226868+J20262030@users.noreply.github.com>
2024-06-02 13:49:59 -0700

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -1,20 +1,26 @@
 import java.net.URL;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
+
 public class Sound{
-    public void playSound() {
+
+    public void playSound(){
         try {
-            URL url = this.getClass().getClassLoader().getResource("diceRolling.wav");
+            URL url = this.getClass().getClassLoader().getResource("diceRolling2.wav");
             Clip clip = AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(url));
             clip.start();
         } catch (Exception exc) {
+            exc.printStackTrace(System.out);
+        }
+    }
+    public void playEndSound(){
+        try{
+            URL url = this.getClass().getClassLoader().getResource("yay.wav");
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(url));
+            clip.start();
+        } catch (Exception exc){
             exc.printStackTrace(System.out);
         }
     }
