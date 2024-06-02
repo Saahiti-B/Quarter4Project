f9814bab691ea1ca9147c743a0cba17a8d885d6f
Saahiti-B <165538451+Saahiti-B@users.noreply.github.com>
2024-06-01 21:28:23 -0700

    Update TriviaQuestions.java

--- TriviaQuestions.java
+++ TriviaQuestions.java
@@ -1,5 +1,11 @@
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.*;
 
 public class TriviaQuestions extends ActionCard{
 //trivia questions are multiple choice
@@ -8,6 +14,7 @@ public class TriviaQuestions extends ActionCard{
     private Color temp;
     private int x;
     private int y;
+    private BufferedImage questionMark;
 
 
     public TriviaQuestions(Color temp, int x, int y){
@@ -17,6 +24,11 @@ public class TriviaQuestions extends ActionCard{
         //questions.add("What is a word for a machine used to make foamy milk? a)roaster b)espresso c)frother");
         //questions.add()
 
+         try{
+            questionMark = ImageIO.read(new File("questionMarkImage.png"));
+        }catch(IOException e){
+            System.out.println(e);
+        }
 
         questions = new String[5];
         questions[0] = "What is a word used to describe cold coffee? a)hot b)iced c) frothy"; //iced
@@ -26,11 +38,9 @@ public class TriviaQuestions extends ActionCard{
         questions[4] = "What is the main ingredient that is used for alertness in coffee? a)chocolate b)milk c)caffeine"; //caffeine
     }
     public void drawCard(Graphics g){
-
-    }
-
-    public void drawType(Graphics g){
-
+        g.drawImage(questionMark,55,207, null);
+        g.drawImage(questionMark,595,207, null);
+        g.drawImage(questionMark,400,505, null);
     }
 
     public void questions(){
